c8d79ea129ebfc1c99f5582e98293cda6f4c78fa
VladDisco07 <32614194+VladDisco07@users.noreply.github.com>
2018-04-16 21:29:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const token = "NDM0Njc5MTg1NjYzODUyNTQ0.DbN-cw.2fuysj6vV8OzRvJQG9-ALFwUEQ4";
 
 client.on("ready", () => {
     console.log("Pregatit!")
-    client.user.setPresence({ game: { name: 'hentai', type: 3 } });
+    client.user.setPresence({ game: { name: 'tate si buci lacrimi si muci', type: 3 } });
 });
 
 const prefix = "!"
